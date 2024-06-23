31973aaed3af7b03efe73c9913d4d4cc77a277a2
Toba Artur <toba.artur@gmail.com>
2024-06-23 16:22:59 +0300

    com

--- online-shop/src/Pages/MainPage/MainPage.jsx
+++ online-shop/src/Pages/MainPage/MainPage.jsx
@@ -2,13 +2,70 @@
 import React from 'react';
 import "./MainPage.scss"
 import { Link } from 'react-router-dom';
+import delivery from './images/delivery.png'
+import checkmark from './images/checkmark.png'
+import purchase from './images/purchase.png'
+import sprout from './images/sprout.png'
 
 
 const MainPage = () => {
   return (
-    <div className='mainpage'>
-      <a className='collection' Link to = "/collection">Viev collection</a>
+    <div className='container'>
+      <div className='mainpage'>
+        <a className='collection' Link to = "/collection">Viev collection</a>
+      </div>
+      <div className='div-panel'>
+        <h1 className='div-panel__title'>
+        What makes our brand different
+        </h1>
+        <div className='panel'>
+        <div className='features'>
+          <img src={delivery} alt="" />
+          <h1>
+          Next day as standard
+          </h1>
+          <p>
+          Order before 3pm and get your order
+          the next day as standard
+          </p>
+        </div>
+        <div  className='features'>
+          <img src={checkmark} alt="" />
+          <h1>
+          Made by true artisans
+          </h1>
+          <p>
+          Handmade crafted goods made with
+real passion and craftmanship
+          </p>
+        </div>
+        <div  className='features'>
+          <img src={purchase} alt="" />
+          <h1>
+          Unbeatable prices
+          </h1>
+          <p>
+          For our materials and quality you won’t find better prices anywhere
+          </p>
+        </div>
+        <div  className='features'>
+          <img src={sprout} alt="" />
+          <h1>
+          Recycled packaging
+          </h1>
+          <p>
+          We use 100% recycled packaging to ensure our footprint is manageable
+          </p>
+        </div>
+      </div>
+      </div>
+      <div className='new-ceramics'>
+        <h1>
+        New ceramics
+        </h1>
+      </div>
     </div>
+   
   );
 };
 
